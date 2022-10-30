9bda7272575427d1f50fe17c42df175d2dd5bdaa
Andkhiz <andkhiz@gmail.com>
2022-10-30 22:18:29 +0300

    fix: change class li tag according to rule 3.1, 3.2, 3.4 in index.html, style.css and app.js

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = "todo__li";
+
     label.innerText=taskString;
     label.className="task";
 
@@ -86,7 +88,7 @@ var editTask=function(){
     var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("todo__li_edit");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -100,7 +102,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("todo__li_edit");
 };
 
 
